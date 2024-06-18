c42b94bb5da9c9d3d59d45193be94ddf62147f25
rosieljdawes <rosieljdawes@gmail.com>
2024-06-18 13:57:21 +0100

    added clock and background

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ class AlienInvasion:
     def __init__(self):
         """initialise the game, and create game resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # set background colour
+        self.bg_color = (230, 230, 230)
     
     def run_game(self):
         """start the main loop for the game"""
@@ -18,8 +22,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # make the most recently drawn screen available
             pygame.display.flip()
+            self.clock.tick(60)
+
 
 if __name__ == '__main__':
     # make a game instance and run the game
